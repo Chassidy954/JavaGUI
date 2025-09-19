b9421474a2eed5afeaea05706754f1804bc9b019
Gabriel Greene <greene_gabriel@yahoo.com>
2025-09-18 20:38:12 -0500

    Add files via upload

---
+++ backend/gradebook-backend/src/test/java/com/gradebookProject/gradebookbackend/GradebookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradebookProject.gradebookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradebookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
